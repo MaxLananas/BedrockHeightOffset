6d61ef37775e88091dbd8ba6b33c272e779edf73
MaxLananas <maxenceponche971@gmail.com>
2026-06-19 14:13:57 -0400

    Refactor BedrockPacketInterceptor for improved message logging and handling

--- src/main/java/fr/buildtheearth/bedrockheightoffset/netty/BedrockPacketInterceptor.java
+++ src/main/java/fr/buildtheearth/bedrockheightoffset/netty/BedrockPacketInterceptor.java
@@ -11,23 +11,23 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class BedrockPacketInterceptor extends ChannelDuplexHandler {
 
     public static final String HANDLER_NAME = "bho_y_interceptor";
 
-    private final UUID             playerUuid;
-    private final OffsetRegistry   registry;
+    private final UUID               playerUuid;
+    private final OffsetRegistry     registry;
     private final BedrockHeightOffset plugin;
-
     private final ChunkTranslator    chunk;
     private final PositionTranslator position;
     private final EntityTranslator   entity;
 
-    // Packet counters for diagnostics
-    private final AtomicLong outboundTotal = new AtomicLong();
-    private final AtomicLong chunksHandled = new AtomicLong();
+    // Log the first 50 outbound object types to understand what we receive
+    private final AtomicLong  msgCount    = new AtomicLong(0);
+    private final AtomicBoolean logged50  = new AtomicBoolean(false);
 
     public BedrockPacketInterceptor(UUID playerUuid, OffsetRegistry registry,
                                     BedrockHeightOffset plugin) {
@@ -41,39 +41,34 @@ public class BedrockPacketInterceptor extends ChannelDuplexHandler {
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise)
-        throws Exception {
+            throws Exception {
+
+        long count = msgCount.incrementAndGet();
+
+        // Log first 100 messages to see what types arrive here
+        if (count <= 100 && plugin.getPluginConfig().isDebug()) {
+            plugin.getLogger().info("[BHO][PIPE] write #" + count
+                + " type=" + msg.getClass().getName()
+                + " simple=" + msg.getClass().getSimpleName());
+        }
 
         PlayerOffsetData data = registry.get(playerUuid);
-
-        if (data != null) {
+        if (data != null && data.getOffset() != 0) {
             String cls = msg.getClass().getSimpleName();
-            outboundTotal.incrementAndGet();
-
-            // Always log chunk packets regardless of offset to diagnose the issue
-            if (cls.equals("LevelChunkPacket")) {
-                chunksHandled.incrementAndGet();
-                if (data.getOffset() != 0) {
-                    plugin.getPluginConfig().debugLog(String.format(
-                        "LevelChunkPacket → offset=%d (%d sec) bY=%.1f",
-                        data.getOffset(), data.offsetSections(), data.currentBedrockY()
-                    ));
-                    msg = chunk.translateChunk(msg, data);
-                }
-            } else if (data.getOffset() != 0) {
-                msg = switch (cls) {
-                    case "MovePlayerPacket"                  -> position.translateMovePlayer(msg, data);
-                    case "RespawnPacket"                     -> position.translateRespawn(msg, data);
-                    case "MoveEntityAbsolutePacket"          -> entity.translateMoveAbsolute(msg, data);
-                    case "AddEntityPacket"                   -> entity.translateAddEntity(msg, data);
-                    case "AddPlayerPacket"                   -> entity.translateAddPlayer(msg, data);
-                    case "UpdateBlockPacket"                 -> chunk.translateBlockUpdate(msg, data);
-                    case "BlockEntityDataPacket"             -> chunk.translateBlockEntity(msg, data);
-                    case "LevelEventPacket"                  -> position.translateLevelEvent(msg, data);
-                    case "SpawnParticleEffectPacket"         -> position.translateParticle(msg, data);
-                    case "NetworkChunkPublisherUpdatePacket" -> position.translateChunkPublisher(msg, data);
-                    default                                  -> msg;
-                };
-            }
+            msg = switch (cls) {
+                case "LevelChunkPacket"                  -> chunk.translateChunk(msg, data);
+                case "MovePlayerPacket"                  -> position.translateMovePlayer(msg, data);
+                case "RespawnPacket"                     -> position.translateRespawn(msg, data);
+                case "MoveEntityAbsolutePacket"          -> entity.translateMoveAbsolute(msg, data);
+                case "AddEntityPacket"                   -> entity.translateAddEntity(msg, data);
+                case "AddPlayerPacket"                   -> entity.translateAddPlayer(msg, data);
+                case "UpdateBlockPacket"                 -> chunk.translateBlockUpdate(msg, data);
+                case "BlockEntityDataPacket"             -> chunk.translateBlockEntity(msg, data);
+                case "LevelEventPacket"                  -> position.translateLevelEvent(msg, data);
+                case "SpawnParticleEffectPacket"         -> position.translateParticle(msg, data);
+                case "NetworkChunkPublisherUpdatePacket" -> position.translateChunkPublisher(msg, data);
+                default                                  -> msg;
+            };
         }
 
         super.write(ctx, msg, promise);
@@ -82,15 +77,10 @@ public class BedrockPacketInterceptor extends ChannelDuplexHandler {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         PlayerOffsetData data = registry.get(playerUuid);
-        if (data != null && data.getOffset() != 0) {
-            String cls = msg.getClass().getSimpleName();
-            if (cls.equals("PlayerAuthInputPacket")) {
-                msg = position.translatePlayerAuthInput(msg, data);
-            }
+        if (data != null && data.getOffset() != 0
+                && msg.getClass().getSimpleName().equals("PlayerAuthInputPacket")) {
+            msg = position.translatePlayerAuthInput(msg, data);
         }
         super.channelRead(ctx, msg);
     }
-
-    public long getOutboundTotal()  { return outboundTotal.get();  }
-    public long getChunksHandled()  { return chunksHandled.get();  }
 }
